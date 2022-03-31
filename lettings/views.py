0b7db418cede9553ab0c127a08a1365448d08727
DenisPil <denis.pilongery@gmail.com>
2022-03-31 15:04:12 +0200

    test autre version

--- lettings/views.py
+++ lettings/views.py
@@ -10,7 +10,6 @@ from .models import Letting
 def lettings_index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
-    
     return render(request, 'lettings/index.html', context)
 
 
