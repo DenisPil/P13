c643bb1286042584e583de1c39d657011ce4a32d
DenisPil <denis.pilongery@gmail.com>
2022-04-03 14:59:20 +0200

    rework des migrations

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,14 @@
-def test_dummy():
-    assert 1
+from django.test import TestCase
+from django.test import Client
+
+from django.urls import reverse
+
+
+class TestHomePage(TestCase):
+
+    def test_main_page(self):
+        path = reverse("index")
+        response = self.client.get(path)
+        assert response.status_code == 200 and \
+            b'<h1>Welcome to Holiday Homes</h1>' \
+            in response.content
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,6 +1,15 @@
 from django.test import TestCase
+from django.test import Client
+
+from django.urls import reverse
 
 
-class Test(TestCase):
-    def test_dummy():
-        assert 1
+class TestProfiles(TestCase):
+
+    def test_profiles_page(self):
+        path = reverse("profiles_index")
+        response = self.client.get(path)
+        print(path, response, '_________________________')
+        assert response.status_code == 200 and \
+            b'<h1>Welcome to Holiday Homes</h1>' \
+            in response.content
