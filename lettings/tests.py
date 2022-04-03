5fcdd36cbf496590740b44418d502ba73d3e2f16
DenisPil <denis.pilongery@gmail.com>
2022-04-03 15:53:11 +0200

    flake8

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 
-from django.contrib.auth.models import User
 from django.urls import reverse
 from .models import Address, Letting
+
 
 class TestProfiles(TestCase):
 
