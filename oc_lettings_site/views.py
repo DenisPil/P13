5154badf6f86048d0ea8741c4394469fab675c18
DenisPil <denis.pilongery@gmail.com>
2022-04-08 10:09:38 +0200

    refactorisation urls

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -11,3 +11,8 @@ from django.shortcuts import render
 # non finibus neque cursus id.
 def index(request):
     return render(request, 'index.html')
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    return division_by_zero
