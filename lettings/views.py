5194054dc127b2413a5aa44dd5cf7f8ffdd476dd
DenisPil <denis.pilongery@gmail.com>
2022-04-13 18:15:23 +0200

    test

--- lettings/views.py
+++ lettings/views.py
@@ -8,7 +8,6 @@ from .models import Letting
 # tempor et, bibendum id arcu. Vestibulum ante ipsum primis in faucibus orci
 # luctus et ultrices posuere cubilia curae; Cras eget scelerisque
 def lettings_index(request):
-
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
 
